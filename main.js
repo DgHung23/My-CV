b42bdaa17f82f0d730b7d99aab37609b7f93b0f8
DgHung23 <hungdangmcn@gmail.com>
2023-07-12 16:39:44 +0700

    add awards

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 function getTime() {
-    var time = new Date()
-    var date = time.getDate()
-    var month = time.getMonth() + 1
-    var year = time.getFullYear()
-    return ('hôm nay là: ngày '+ date + ' tháng ' + month + ' năm ' + year)
+    var time = new Date();
+    var date = time.getDate();
+    var month = time.getMonth() + 1;
+    var year = time.getFullYear();
+    return "hôm nay là: ngày " + date + " tháng " + month + " năm " + year;
+}
+console.log(getTime());
+
+// ---------Object structure-------------
+function user(firstName, lastName, email, avatar) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.avatar = avatar;
+    this.getFullName = function () {
+        return this.firstName + " " + this.lastName;
+    };
 }
 
-console.log(getTime())
+// --------------Object---------------
+var admintrator = new user("Dg", "Hung", "test@gmail.com", "img.com");
+var user = new user("Son", "Dang", "test2@gmail.com", "img2.xyz");
 
+console.log("admin: ", admintrator.firstName, admintrator.lastName);
+console.log("user_test: ", user.getFullName());
